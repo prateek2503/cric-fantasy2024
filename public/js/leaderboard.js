218c1d04bb51e37d5a2e70035415b5783ccb2a51
Kaushik <ksahoo@ivycomptech.com>
2021-04-14 13:03:28 +0530

    show bids

--- public/js/leaderboard.js
+++ public/js/leaderboard.js
@@ -8,4 +8,12 @@
         }
     });
 
+    $.get("bids", function(data) {
+        if (data) {
+            $.each(data, function(index, bid) {
+                 $('#bids').append(JSON.stringify(bid) + "<br/>");
+            });
+        }
+    });
+
 })();
