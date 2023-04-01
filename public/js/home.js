03c96ae4ed3257984886e86eada343689f9390f6
prateek2503 <prateek25.03@gmail.com>
2023-04-01 18:27:54 +0530

    ipl 2023

--- public/js/home.js
+++ public/js/home.js
@@ -44,10 +44,10 @@
                         <div class="input-group">
                             <label class="input-group-text" for="bid-select-${match.id}">Boli</label>
                             <select class="form-select" id="bid-select-${match.id}">
-                                <option value="150" selected>150</option>
-                                <option value="300">300</option>
-                                <option value="450">450</option>
-                                <option value="600">600</option>
+                                <option value="50" selected>50</option>
+                                <option value="100">100</option>
+                                <option value="150">150</option>
+                                <option value="200">200</option>
                             </select>
                         </div>
                         <div class="float-end">
